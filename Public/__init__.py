b3bae644c4fd6e06df3a05df2b9030e9c2f36e7e
jiangtao <16345@hnjing.com>
2018-06-01 09:42:53 +0800

    rm

--- Public/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2018/05/29 17:34
-# @Author  : jt
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm
--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-""" 
-@author: jt
-@file: __init__.py 
-@time: 2018/05/29 14:17
-"""  
